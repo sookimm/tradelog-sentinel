1056d78080f22f340b3cda8161f3e856095bf7b8
sookimm <su241383@gmail.com>
2026-05-17 15:31:04 +0900

    Add GET logs API endpoint

--- src/main/java/com/sooyeon/tradelogsentinel/controller/LogController.java
+++ src/main/java/com/sooyeon/tradelogsentinel/controller/LogController.java
@@ -5,6 +5,8 @@ import com.sooyeon.tradelogsentinel.entity.LogEntry;
 import com.sooyeon.tradelogsentinel.repository.LogEntryRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/logs")
 public class LogController {
@@ -17,12 +19,16 @@ public class LogController {
 
     @PostMapping
     public LogEntry createLog(@RequestBody CreateLogRequest request) {
-
         LogEntry log = new LogEntry(
                 request.getLevel(),
                 request.getMessage()
         );
 
         return logEntryRepository.save(log);
+    }
+
+    @GetMapping
+    public List<LogEntry> getAllLogs() {
+        return logEntryRepository.findAll();
     }
 }
